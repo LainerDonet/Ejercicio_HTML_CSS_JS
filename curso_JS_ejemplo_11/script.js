62e6c4cb8bc89efd6c6b01e521c0de306fbd328f
Lainer Donet <lainerdonet87@gmail.com>
2024-12-09 15:02:50 -0600

    por terminar 11

---
+++ curso_JS_ejemplo_11/script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container");
+const input = document.querySelector(".search");
+const button = document.getElementById("_button");
+const searchIcon = document.querySelector(".fa-search");
+const closeIcon = document.querySelector(".fa-time");
